65f2882dd3fc9197eac1eec06ea9595e2f1e2a59
Johanna Snow <johannadsnow@gmail.com>
2016-09-08 12:40:28 -0500

    first commit

---
+++ server/app.js
@@ -0,0 +1,14 @@
+var express = require( 'express' );
+var app = express();
+
+
+//spin up the server
+app.listen( '8080', 'localhost', function( req, res ){
+  console.log( 'server listening on port 8080' );
+}); //end app.listen
+
+
+app.get( '/', function( req, res ){
+  console.log( 'base URL hit' );
+  res.send( 'hello world from server!' );
+});
